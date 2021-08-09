a004ff8e8a0153307e9fb8aab6ee647f41733557
Heon_9u <frtt0608@naver.com>
2021-08-09 16:39:06 +0900

    210809|hg|가장긴팰린드롬|매내처 알고리즘|시간복잡도 O(N)

---
+++ Heon9u/BJ/Main.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+// Manacher algo
+public class Main {
+    static int[] table;
+
+    public static void setManacherTable(char[] chars) {
+        int size = chars.length;
+        table = new int[size];
+        int scope=0, center=0;
+
+        for(int i=0; i<size; i++) {
+            if(i > scope) 
+                table[i] = 0;
+            else 
+                table[i] = Math.min(table[2*center-i], scope-i);
+
+            while(i-table[i]-1>=0 && i+table[i]+1<size &&
+                        chars[i-table[i]-1] == chars[i+table[i]+1]) {
+                table[i] += 1;
+            }
+
+            if(scope < i+table[i]) {
+                scope = i+table[i];
+                center = i;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char[] inputs = br.readLine().toCharArray();
+
+        StringBuilder sb = new StringBuilder();
+        for(char chr: inputs) {
+            sb.append("#").append(chr);
+        }
+
+        String str = sb.append("#").toString();
+        setManacherTable(str.toCharArray());
+
+        int maxCount = Arrays.stream(table).max().getAsInt();
+        System.out.println(maxCount);
+    }
+}
