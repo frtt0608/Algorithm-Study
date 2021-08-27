4da2951d1096681f6d4ca18606fc8c3a2efb09c5
jeong369 <injeong319@gmail.com>
2021-08-27 11:45:46 +0900

    210827|ij|programmers - dfs - 덩어리 찾기

--- injeong/programmers/graph_네트워크.py
+++ injeong/programmers/graph_네트워크.py
@@ -6,31 +6,35 @@
 
 if __name__ == '__main__':
     def solution(n, computers):
+        # graph로 바꿔주기
         graph = {}
         for i in range(n):
             graph[i] = []
             for j in range(n):
                 if i!=j and computers[i][j]==1:
                     graph[i].append(j)
-        # print(graph)
 
 
         def dfs(v, visited=[]):
+            # 방문 체크
             visited.append(v)
+            # 연결된 컴퓨터 중
             for w in graph[v]:
+                # 방문하지 않은 컴퓨터 체크
                 if w not in visited:
-                    visited = dfs(w, visited)
+                    visited = dfs(w, visited) # 방문 기록 갖고 가야 함
             return visited
 
         answer, i = 0, 0
         chk = []
         while set(chk) != set([x for x in range(n)]):
+            # 한 덩어리로 묶이지 않은 컴퓨터
             if i not in chk:
                 chk = dfs(i)
                 answer += 1
+            # 묶은 컴퓨터는 패쓰
             else:
                 i += 1
-            # print(chk)
 
         return answer
 
