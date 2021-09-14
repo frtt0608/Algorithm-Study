22d26c4b149e302d542f60b51a9a8839298250ee
jeong369 <injeong319@gmail.com>
2021-09-14 12:14:12 +0900

    210914|ij|swexpert-자리수에 맞는 글자 찾기-규칙을 찾아서 최대한 식으로 빼준 다음 찾는 부분은 최소화하기

--- injeong/swExpert/12369. 일련번호 붙이기.py
+++ injeong/swExpert/12369. 일련번호 붙이기.py
@@ -6,42 +6,31 @@ A B AA AB BA BB AAA AAB ABA ABB BAA BAB(12) BBA BBB
 -> A와 B로 만들 수 있는 중복순열
 l개로 만든다면, 1자리=l개, 2자리=l*l개, 3자리=l*l*l개
 '''
-
-import sys
-sys.setrecursionlimit(1000000) # 재귀 깊이 늘려주기(파이썬은 기본으로 1000으로 제한)
-
 if __name__ == '__main__' :
     for tc in range(1, int(input())+1):
         word, N = input().split(' ')
         word, l, N = sorted(list(word)), len(word), int(N)
-        # print(word, l, N)
-        # l개로 1개부터 중복순열 만들어서 N번째 글자 찾기
 
-        result = []
-        def perm(n, r, N): # n개 중 r개(중복가능) cnt번째
-            global cnt, answer
-            if r==0 :
-                cnt += 1
-                if cnt == N :
-                    answer = ''.join(result)
-                    return
-                return
-            for i in range(n):
-                result.append(word[i])
-                perm(n, r-1, N)
-                result.pop()
-                if answer != '' : break
-            return
-        
-        # 몇자리 찾으면 되는지 자리수 k 찾기
-        i, s = 1, 0
-        while s < N :
-            s += l**i
-            i += 1
-        cnt, k = s-l**(i-1), i-1
+        k = 0
+        while N > 0:
+            k += 1
+            if N - l**k > 0:
+                N -= l**k
+                continue
+            else:
+                break
+        # k자리수로 시작/N만큼 가면 됨
 
         answer = ''
-        perm(l, k, N)
+        for i in range(k): # 자리수만큼 돌면서
+            jaja = (l**(k-i-1)) # 첫번째 자리 고정이면 경우의 수는 l**(l-1)
+            for idx, alp in enumerate(word): # 어떤 수가 고정인 지 찾는 것.
+                if N - jaja > 0: # A부터 고정인 경우를 빼가면서 정리
+                    N -= jaja
+                    continue
+                else: 
+                    answer += alp
+                    break
         
         print('#{} {}'.format(tc, answer))
 
