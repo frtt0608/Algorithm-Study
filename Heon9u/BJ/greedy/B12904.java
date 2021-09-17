39e14ff658709bd10600e1064a850c1b503a4493
Heon_9u <frtt0608@naver.com>
2021-09-17 13:57:34 +0900

    210917|hg|그리디|A와 B|역으로 생각

--- Heon9u/BJ/greedy/B12904.java
+++ Heon9u/BJ/greedy/B12904.java
@@ -1,7 +1,11 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+
+// A와 B
+// 뒤집거나 문자열 자르는 과정을 인덱스 + flag로 대체
+// 아래 코드에서는 람다식 활용과 가독성을 위해 substring으로 진행
+public class B12904 {
 
     public static int checkChangeStoT(String S, String T) {
         boolean isReverse = false;
