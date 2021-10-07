c29fe943f0b815d3929a95f68a9b526a00bd57a8
jeong369 <injeong319@gmail.com>
2021-10-07 18:02:44 +0900

    211007|ij|programmers|Hash|dict를 활용하여 효율성 보완

--- injeong/programmers/practice/hash_lv2_전화번호목록.py
+++ injeong/programmers/practice/hash_lv2_전화번호목록.py
@@ -8,21 +8,41 @@
 -> 번호끼리 접두어면 false, 아니면 true 반환해라.
 '''
 # 정확성(100%)과 효율성(50%)을 따지는 문제였음.
+# def solution(phone_book):
+#     phone_book.sort(key=len) # 정렬한 다음,
+#     answer = True
+#     i = 0
+#     j = i+1
+#     while i<len(phone_book)-1:
+#         l = len(phone_book[i]) # 기준 번호의 길이
+#         if phone_book[i] == phone_book[j][0:l]: # 비교대상 번호의 접두어인지
+#             answer = False
+#             break
+#         else:
+#             j+=1
+#             if j == len(phone_book):
+#                 i += 1
+#                 j = i+1
+#     return answer
+
 def solution(phone_book):
-    phone_book = sorted(phone_book, key=lambda x:len(x)) # 정렬한 다음,
+    phone_book.sort(key=len) # 정렬한 다음,
+    print(phone_book)
+    dicts = {}
+    len_lists = []
     answer = True
-    i = 0
-    j = i+1
-    while i<len(phone_book)-1:
-        l = len(phone_book[i]) # 기준 번호의 길이
-        if phone_book[i] == phone_book[j][0:l]: # 비교대상 번호의 접두어인지
-            answer = False
-            break
-        else:
-            j+=1
-            if j == len(phone_book):
-                i += 1
-                j = i+1
+    for i in range(len(phone_book)):
+        # add to dictionary & list
+        now_phone = phone_book[i]
+        dicts[now_phone] = []
+        for l in len_lists:
+            if now_phone[0:l] != now_phone and now_phone[0:l] in dicts.keys():
+                dicts[now_phone].append(now_phone[0:l])
+                answer = False
+        n = len(now_phone)
+        if n not in len_lists:
+            len_lists.append(n)
+
     return answer
 
 print(solution(["119", "97674223", "1195524421"]), False)
