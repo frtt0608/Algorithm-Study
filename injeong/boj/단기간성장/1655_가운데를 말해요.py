c4201b34a7a56eb506d88b6f57343ce3e55f3cd2
jeong369 <injeong319@gmail.com>
2021-11-03 15:20:03 +0900

    211103|ij|이분탐색법 정리-시간초과->우선순위큐로 푸는 문제라고 함

--- injeong/boj/단기간성장/1655_가운데를 말해요.py
+++ injeong/boj/단기간성장/1655_가운데를 말해요.py
@@ -9,13 +9,27 @@ sys.stdin = open("input.txt", "r")
 
 if __name__=='__main__':
     N = int(input())
-    report = []
-    for i in range(N):
+    report = [-10000, 10000]
+    mid_idx = 0
+    l = 2
+    for k in range(N):
         n = int(input())
-        report.append(n)
-        report = sorted(report)
-        l = len(report)
-        if l%2:
-            print(report[l//2])
+        # print(report)
+        l += 1
+        if n >= report[mid_idx]:
+            for i in range(mid_idx+1, len(report)):
+                if n <= report[i]:
+                    report.insert(i, n)
+                    if l%2: # 홀수면 / 짝수면 그대로
+                        mid_idx += 1
+                    break
         else:
-            print(min(report[l//2-1], report[l//2]))
+            for i in range(mid_idx-1, -1, -1):
+                if report[i] < n :
+                    report.insert(i+1, n)
+                    if not l%2: # 짝수면,
+                        mid_idx -= 1
+                    else: # 홀수면,
+                        mid_idx += 1
+                    break
+        print(report[mid_idx])
