774f9d1915b5c954135de51cddb446482159f6f4
jeong369 <injeong319@gmail.com>
2021-11-09 09:23:10 +0900

    211109|ij|수학문제-규칙 찾아서 재귀 돌리니 효율성 통과

--- injeong/programmers/lv2_124 나라의 숫자.py
+++ injeong/programmers/lv2_124 나라의 숫자.py
@@ -7,39 +7,27 @@ Question) n을 124 나라에서 사용하는 숫자로 바꾼 값을 return
 
 def solution(n):
     elements = ['1', '2', '4'] # [1,2,4]를 중복해서 k번 뽑는 것
-    # 등비수열의 합공식 이용
-    k = 1
-    while 3**(k+1) < 2*n+3:
-        k += 1
-    if k!=1:
-        r = n-((3**k-3)//2) # r=3이고 k-1개의 합을 빼주면 k자리수+몇번째 뽑아야 하는지 나옴
-    else:
-        r = n
-    # print(k, r)
-
-    def comb(k, curr):
-        nonlocal r, answer
-        # 3개 중 k자리수 뽑기/현재 curr개 뽑음
-        if curr==k:
-            r -= 1
-            if r==0:
-                answer = ''.join(result)
-            return 
-        for i in range(3):
-            result.append(elements[i])
-            comb(k, curr+1)
-            result.pop(-1)
-        return
-
+    # 재귀 이용
+    # ex) 15 => 14 = (4,2) => 나머지 2는 '4'
+    #                 4-1=3 => (1, 0) => 나머지 0은 '1'
+    #                           1-1=0 => (0, 0) => 나머지 0은 '1'
+    # 이런 규칙으로 재귀 가능.
+    def recursion(n):
+        nonlocal answer
+        if n<0:
+            return
+        q, r = divmod(n, 3)
+        answer = elements[r] + answer
+        return recursion(q-1)
+    
     answer = ''
-    result = []
-    comb(k, 0)
+    recursion(n-1)
 
     return answer
 
-# print(solution(1),1)
-# print(solution(2),2)
-# print(solution(3),4)
-# print(solution(4),11)
-# print(solution(13),111)
-print(solution(500000000),)
+print(solution(1),1)
+print(solution(2),2)
+print(solution(3),4)
+print(solution(4),11)
+print(solution(15),114)
+# print(solution(500000000),)
