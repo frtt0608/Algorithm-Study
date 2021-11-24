50ffa891ea0c7c219649cba181243fc6e100ad59
jeong369 <injeong319@gmail.com>
2021-11-24 20:10:15 +0900

    211124|ij|programmers|문자열-간단하게 생각하기-stack의 원리와 문제 이해하기

--- injeong/programmers/lv2_짝지어제거하기.py
+++ injeong/programmers/lv2_짝지어제거하기.py
@@ -1,24 +1,16 @@
 def solution(s):
-    going = True
-    while going:
-        going = False
-        lists = {}
-        for word in s:
-            if lists.get(word):
-                lists[word] += 1
-            else:
-                lists[word] = 1
-        sort_lists = sorted(lists.items(), key=lambda x: x[1], reverse=True)
-        for word in sort_lists:
-            w = word[0]*2
-            tmp_s = s.replace(w, '')
-            if s != tmp_s: # 변화가 있으면
-                s = tmp_s
-                going = True
-    if not s:
+    stack = []
+    for word in s:
+        if not stack or stack[-1]!=word:
+            stack.append(word)
+        elif stack[-1]==word:
+            stack.pop(-1)
+
+    if not stack:
         return 1
     else:
         return 0
 
 print(solution("baabaa"), 1)
 print(solution("cdcd"), 0)
+print(solution("daddccada"), 0)
